13d76f12582ec3e204c3b70131a4fac547b0da53
ynb925 <ynb4mail@gmail.com>
2022-04-14 10:56:45 +0300

    ynb single prg for check wrt code and turn on it frome MAIN

---
+++ src/com/ynb/lesons_4/LesExam_4.java
@@ -0,0 +1,20 @@
+package com.ynb.lesons_4;
+
+/**
+ * ynb single prg for check wrt code and turn on it frome MAIN
+ */
+public class LesExam_4 {
+    public static void main(String[] args) {
+        int sum = getSum();
+        System.out.println(sum);
+
+    }
+
+    public static int getSum() {
+        int a = 10;
+        int b = 16;
+        return a + b;
+
+    }
+}
+
