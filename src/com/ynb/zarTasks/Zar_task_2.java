6b2fdc4dd00893f99c5ea75a9469bc9c3ccd0d03
ynb925 <ynb4mail@gmail.com>
2022-04-16 00:44:02 +0300

    save

--- src/com/ynb/zarTasks/Zar_task_2.java
+++ src/com/ynb/zarTasks/Zar_task_2.java
@@ -15,47 +15,16 @@ package com.ynb.zarTasks;
 
 public class Zar_task_2 {
     public static void main(String[] args) {
-        int start = -5;
-        int finish = 8;
-        int res = 9%2;
+        int fact = 7;
+        for (int i = fact; i > 0; i--) {
+            int multi = (fact * fact - i);
+            int res = fact  * multi;
+            System.out.println(res);
 
-
-        for (int i = start; i <= finish; i++) {
-            //System.out.print(i);
-            System.out.println(i);
 
         }
-
-
-//        int fact = 5;
-//        int result = 0;
-
-
-//        for (int f = 0; fact > f; f--) {
-//            result = fact * (fact - 1);
-//        }
-//        System.out.println(result);
-/*
-        for (int i = 5; i > 0; i--) {
-            System.out.println(i);
-        }*/
     }
 }
 
-
-
-/*public class Zar_task_2 {
-    public static void main(String[] args) {
-        int factorial = factorial(5);
-        System.out.println(factorial);
-    }
-
-    private static int factorial(int i) {
-        if (i == 1) {
-            return 1;
-        }
-        return i * factorial(i - 1);
-    }
-}*/
 
 
