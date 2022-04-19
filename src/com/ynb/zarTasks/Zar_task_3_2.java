196a55bdbd84aab557fd076d1e5253b48c1482d5
ynb925 <ynb4mail@gmail.com>
2022-04-19 14:08:37 +0300

    tsk 3_2 done  !!! dbt can be upgrade

---
+++ src/com/ynb/zarTasks/Zar_task_3_2.java
@@ -0,0 +1,25 @@
+package com.ynb.zarTasks;
+
+import java.util.Scanner;
+
+/**
+ * Задание 3.2 Во многих странах для измерения небольших размеров используется не только единица длины «сантиметр»,
+ * но и такая единица, как «дюйм>> (1дюйм=2.54 сантиметра). Напишите класс, который принимает с клавиатуры значение,
+ * равное размеру в сантиметрах, и затем подсчитывает и выводит на экран это же значение, но в дюймах.
+ */
+
+public class Zar_task_3_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число в 'сантиметрах' ");
+        if (scanner.hasNextInt()) {
+            int value = scanner.nextInt();
+            float inchPrint = (float) (value * 2.54);
+            System.out.println(inchPrint + " дюйм");
+        } else {
+            System.out.println("Не тот тип данных, введите число!");
+        }
+        scanner.close();
+    }
+}
+
