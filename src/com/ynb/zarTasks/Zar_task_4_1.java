163ddbe84ea1adbf16574dd529b6f63cb0aa7279
ynb925 <ynb4mail@gmail.com>
2022-04-19 23:49:37 +0300

    tsk 4_1 start job

---
+++ src/com/ynb/zarTasks/Zar_task_4_1.java
@@ -0,0 +1,16 @@
+package com.ynb.zarTasks;
+
+import java.util.Scanner;
+
+/**
+ * Задание 4.1 Напишите класс, который принимает с клавиатуры целое положитель ное двузначное число и выводит на экран его цифры,
+ * разделенные знаком «пробел».
+ */
+
+public class Zar_task_4_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
