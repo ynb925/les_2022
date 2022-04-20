3b1b69c66c3965a9216fb4ac129cd4043d8d91c1
ynb925 <ynb4mail@gmail.com>
2022-04-20 19:43:16 +0300

    tsk 4_1 start  \ test code

--- src/com/ynb/zarTasks/Zar_task_4_1.java
+++ src/com/ynb/zarTasks/Zar_task_4_1.java
@@ -10,7 +10,14 @@ import java.util.Scanner;
 public class Zar_task_4_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        System.out.println("Введите 2х-ное число: ");
+        int n = scanner.nextInt();
+        int m = n / 10; //кол-во десятков
+        int c = n - m * 10; //количество единиц
+        char c1 = (char) scanner.nextInt();
+        String res = String.valueOf (c1);
+        System.out.println("Количество десятков: " + m);
+        System.out.println("Количество единиц: " + c);
+        System.out.println("Результат: " + res);
     }
 }
