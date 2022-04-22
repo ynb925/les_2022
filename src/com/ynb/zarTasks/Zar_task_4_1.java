0480ba320eb0c68be37b29feb43d33021eeb413e
ynb925 <ynb4mail@gmail.com>
2022-04-22 17:46:09 +0300

    work done \ wait qa-test  !!!

--- src/com/ynb/zarTasks/Zar_task_4_1.java
+++ src/com/ynb/zarTasks/Zar_task_4_1.java
@@ -12,12 +12,18 @@ public class Zar_task_4_1 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите 2х-ное число: ");
         int n = scanner.nextInt();
-        int m = n / 10; //кол-во десятков
-        int c = n - m * 10; //количество единиц
-        char c1 = (char) scanner.nextInt();
-        String res = String.valueOf (c1);
-        System.out.println("Количество десятков: " + m);
-        System.out.println("Количество единиц: " + c);
-        System.out.println("Результат: " + res);
+        String str = Integer.toString(n);
+        char c = 0;
+        char b = 0;
+
+        System.out.println("int to str:  " + str); // out result is convert INT to STR
+
+        for (int i = 0; i < str.length(); i++) {
+            c = str.charAt(0);
+            b = str.charAt(1);
+            System.out.println(c + "  " + b);   // out result is convert  STR to char and add "_"
+        }
     }
 }
+
+
