51dc99af0f214ba4919b6796c354398380aa9690
ynb925 <ynb4mail@gmail.com>
2022-04-29 11:34:15 +0300

    wrk finish

--- src/com/ynb/lesons_2/LesExam_2.java
+++ src/com/ynb/lesons_2/LesExam_2.java
@@ -1,12 +1,12 @@
 package com.ynb.lesons_2;
 
 public class LesExam_2 {
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
         System.out.println("Привет!");
         float x = (float) (2 - 1.2);
         float y = x;
-        System.out.println(x);
+        System.out.println(y);
     }
 
 }
