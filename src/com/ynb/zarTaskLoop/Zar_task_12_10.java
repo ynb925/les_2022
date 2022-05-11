2da48b82176b9434354352c27d900793435ce29f
ynb925 <ynb4mail@gmail.com>
2022-05-11 22:45:53 +0300

    wrk done \\ no qa pass !!!

---
+++ src/com/ynb/zarTaskLoop/Zar_task_12_10.java
@@ -0,0 +1,20 @@
+package com.ynb.zarTaskLoop;
+
+import java.util.Scanner;
+
+/**
+ * Задание 12.10  Контрольная работа считается «провальной», если более половины учеников получили неудовлетворительные оценки.
+ * Напишите класс, который принимает с клавиатуры число учеников в группе, а затем -оценку каждого ученика.
+ * Класс должен определить, была ли контрольная «провальной».
+ * ПС в класе может быть и 4-5 человек.
+ */
+
+public class Zar_task_12_10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" how match we do: ");
+        int number = scanner.nextInt();
+
+
+    }
+}
