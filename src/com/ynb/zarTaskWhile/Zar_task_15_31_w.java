73ff7dbc9f6b60d12c1328a3254457fd8192e459
ynb925 <ynb4mail@gmail.com>
2022-05-15 23:19:23 +0300

    wrk done \\   qa pass !!!

--- src/com/ynb/zarTaskWhile/Zar_task_15_31_w.java
+++ src/com/ynb/zarTaskWhile/Zar_task_15_31_w.java
@@ -9,5 +9,19 @@ import java.util.Scanner;
 
 
 public class Zar_task_15_31_w {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" count of enter nums: ");
+        int scan;
+        int countIter = 0;
 
+        while (countIter < 5) {
+            scan = scanner.nextInt();
+            if (scan % 2 == 0) {
+                countIter++;
+            }
+        }
+        System.out.println("even nums is < 5 times");
+    }
 }
+
