eeaa446a6f9214163e05517ed11edf6662369ef8
ynb925 <ynb4mail@gmail.com>
2022-05-15 22:32:00 +0300

    wrk done \\   qa pass !!!

---
+++ src/com/ynb/zarTaskWhile/Zar_task_15_28_w.java
@@ -0,0 +1,30 @@
+package com.ynb.zarTaskWhile;
+
+import java.util.Scanner;
+
+/**
+ * Задание 15.28 типичная задача для вайла))))
+ * Напишите класс, который принимает с клавиатуры целое число и подсчитывает число цифр в нем (количество цифр в числе).
+ * Результат подсчета должен быть выведен на экран. Например, для чис ла 534 на экран будет выведено 3 цифры.
+ */
+
+
+public class Zar_task_15_28_w {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter nums 2x symbols: ");
+        int inputNum = scanner.nextInt();
+
+        int division = 1;
+        int sum = 0;
+        int countNums = 0;
+        while (inputNum / division != 0) {             // (inpun != 0)
+
+            sum = sum + (inputNum / division) % 10;    // sum += inputNum % 10;
+            division = division * 10;                  // inputNum /= 10;
+
+            countNums++;
+        }
+        System.out.println(countNums);
+    }
+}
