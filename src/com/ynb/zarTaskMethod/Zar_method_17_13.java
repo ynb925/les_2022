069033872b14de409c3f2db44d26ebc851966808
ynb925 <ynb4mail@gmail.com>
2022-05-18 15:42:22 +0300

    wrk done \\   qa pass !!!

--- src/com/ynb/zarTaskMethod/Zar_method_17_13.java
+++ src/com/ynb/zarTaskMethod/Zar_method_17_13.java
@@ -9,6 +9,49 @@ import java.util.Scanner;
  */
 public class Zar_method_17_13 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println(" wrt 1st num : ");
+        int scan1st = scanner.nextInt();
+
+        System.out.println(" wrt 2nd num : ");
+        int scan2nd = scanner.nextInt();
+
+        System.out.println(getTrue(scan1st, scan2nd));
+
+    }
+
+    public static int checkNum1(int scan1st) {
+        int div = 1;
+        int value1 = 0;
+
+        while (scan1st / div != 0) {
+            div = div * 10;
+            value1++;
+        }
+        return value1;
+    }
+
+    public static int checkNum2(int scan2nd) {
+        int div = 1;
+        int value2 = 0;
+
+        while (scan2nd / div != 0) {
+            div = div * 10;
+            value2++;
+        }
+        return value2;
+    }
+
+    public static boolean getTrue(int value1, int value2) {
+        if (value1 > value2) {
+            return false;
+        }
+        if (value1 < value2) {
+            return false;
+
+        } else {
+            return true;
+        }
     }
 }
