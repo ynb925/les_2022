22c2c3c3fe19b4477dc914de32de18d3262e852c
ynb925 <ynb4mail@gmail.com>
2022-05-19 11:32:13 +0300

    wrk done \\   qa pass !!!

--- src/com/ynb/lesons_0/LesExam_0.java
+++ src/com/ynb/lesons_0/LesExam_0.java
@@ -1,11 +1,19 @@
 package com.ynb.lesons_0;
 
-/*  Задача 1.1.1 (есть в учебнике, тема 1).
- *      Создайте новый проект и запустите в main распечатку
- */
+import java.util.Scanner;
+
 
 public class LesExam_0 {
     public static void main(String[] args) {
-        System.out.printf("new prgjkt");
+        Scanner scanner = new Scanner(System.in);
+        int scan;
+        do {
+            scan = scanner.nextInt();
+            if (scan != 123) {
+                System.out.println("cod not correct. try again");
+            } else {
+                System.out.println("cod accepted ");
+            }
+        } while (true);
     }
 }
