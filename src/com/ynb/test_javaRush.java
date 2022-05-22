d636bf05830e1ff0f7d537cd7350ba2c79500556
ynb925 <ynb4mail@gmail.com>
2022-05-22 21:20:49 +0300

    wrk done \\  no QA pass !!!

---
+++ src/com/ynb/test_javaRush.java
@@ -0,0 +1,96 @@
+package com.ynb;
+
+import java.util.Scanner;
+
+public class test_javaRush {
+    public static void main(String[] args) {
+
+
+    }
+}
+  /*      int sum = 0;
+        int i = 0;
+        while (i <= 100) {
+            if ((i % 3) == 0) {
+                i++;
+                continue;
+            }
+            sum = sum + i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}*/
+
+/*        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String s = scanner.nextLine();
+
+            if (s.equals("enough")) {
+                break;
+            }
+            System.out.println(s);
+        }
+    }
+}*/
+
+
+
+
+  /*      //напишите тут ваш код
+        int a = 0;
+        while (a < 10) {
+            a++;
+            int b = 0;
+            System.out.print("Б");
+            while (b < 19) {
+                if (a > 1 && a < 10 && b < 18) {
+                    System.out.print(" ");
+                    b++;
+                } else {
+                    System.out.print("Б");
+                    b++;
+                }
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+  /*  public static void main(String[] args) {
+        int a = 0;
+        while (a < 5) {
+            a++;
+            int b = 0;
+            while (b < 10) {
+                System.out.print("Q");
+                b++;
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+
+
+
+   /* public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        boolean isEnter = false;
+
+        while (!isEnter) {
+            int a = scanner.nextInt();
+            sum = sum + a;
+
+            if (scanner.hasNextLine()) {
+                isEnter = scanner.hasNext("ENTER");
+            }
+        }
+        System.out.println(sum);
+    }
+}*/
+
+
