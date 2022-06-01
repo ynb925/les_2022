eeeba746be969b4865d498608fb5ee16bf589369
ynb925 <ynb4mail@gmail.com>
2022-06-01 17:35:10 +0300

    wrk done \\  no QA pass !!!

--- src/com/ynb/zarArrays/rushArrays_3.java
+++ src/com/ynb/zarArrays/rushArrays_3.java
@@ -13,7 +13,6 @@ public class rushArrays_3 {
         arr1 = new int[]{1, 3, 33, 44, 66, 7, 8, 32, 45, 11};
         arr2 = new int[]{36, 3, 24, 11, 66, 7, 34, 32, 17, 2};
 
-
         System.out.println(checkLengthArr(arr1, arr2));
     }
 
