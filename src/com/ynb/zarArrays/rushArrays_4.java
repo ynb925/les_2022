0feb1c218fb491a5223fd49995ab0065d1030058
ynb925 <ynb4mail@gmail.com>
2022-06-01 17:43:03 +0300

    wrk done \\  no QA pass !!!

---
+++ src/com/ynb/zarArrays/rushArrays_4.java
@@ -0,0 +1,23 @@
+package com.ynb.zarArrays;
+
+/**
+ * сравнить сумму двух массивов
+ */
+
+public class rushArrays_4 {
+    public static void main(String[] args) {
+
+        int[] arr1 = new int[]{1, 3, 33, 44, 66, 7, 8, 32, 45, 11};
+        int[] arr2 = new int[]{36, 3, 24, 11, 66, 7, 34, 32, 17, 2};
+
+    }
+
+    public static int checkSum(int[] arrs) {
+
+        for (int i = 0; i < arrs.length; i++) {
+
+
+        }
+    }
+
+}
