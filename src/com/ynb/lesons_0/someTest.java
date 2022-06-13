59a5607a974f1a3c16476606731cb80d980c1758
ynb925 <ynb4mail@gmail.com>
2022-06-13 13:45:38 +0300

    wrk done \\  no QA pass !!!

---
+++ src/com/ynb/lesons_0/someTest.java
@@ -0,0 +1,29 @@
+package com.ynb.lesons_0;
+
+public class someTest {
+    public static void main(String[] args) {
+
+
+    }
+}
+
+/*        // test while
+        int b = 16;
+        int c = 0;
+        while (c < b) {
+            c++;
+            System.out.print(c + " ");  // will print res  1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+        }
+    }
+}*/
+
+
+/*        // test for
+
+        int a = 17;
+
+        for (int i = 0; i < a; i++) {
+            System.out.print(i + " ");  // will print res  0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+        }
+    }
+}*/
