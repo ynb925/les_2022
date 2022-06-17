bb758ee01232052e2909fcfe84e63985324a50c3
ynb925 <ynb4mail@gmail.com>
2022-06-17 16:46:35 +0300

    wrk done \\  no QA pass !!!

--- src/com/ynb/zarArrays/Arr18_16.java
+++ src/com/ynb/zarArrays/Arr18_16.java
@@ -1,5 +1,8 @@
 package com.ynb.zarArrays;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Задание 18.16 Напишите класс, который заполняет случайными двузначными числами массив,
  * размер которого при инициализации вводится с клавиатуры.  После заполнения класс должен
@@ -11,5 +14,40 @@ package com.ynb.zarArrays;
 public class Arr18_16 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.println(" wrk cycle count: ");
+        int countNum = scanner.nextInt();
+        int beforeNum = 0;
+
+        for (int i = 0; i < countNum; i++) {
+
+            System.out.println(" wrt num: ");
+            int enterNum = scanner.nextInt();
+
+            System.out.println("eNum | " + enterNum + "    " + "befNum | " + beforeNum + " _________ | count |" + i);
+            beforeNum = enterNum;
+        }
     }
 }
+
+/*
+    Scanner scanner = new Scanner(System.in);
+    Random random = new Random();
+
+        System.out.println(" wrk cycle count: ");
+                int countNum = scanner.nextInt();
+                int beforeNum = 0;
+
+                for (int i = 0; i < countNum; i++) {
+
+        System.out.println(" wrt num: ");
+        int enterNum = scanner.nextInt();
+
+        System.out.println("eNum | " + enterNum + "    " + "befNum | " + beforeNum + " _________ | count |" + i);
+        beforeNum = enterNum;
+        }
+        }
+        }
+*/
