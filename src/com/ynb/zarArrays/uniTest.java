1e61675b340dc755f3b81184c39f29f9c140438d
ynb925 <ynb4mail@gmail.com>
2022-06-17 22:29:02 +0300

    wrk done \\  no QA pass !!!

--- src/com/ynb/zarArrays/uniTest.java
+++ src/com/ynb/zarArrays/uniTest.java
@@ -3,6 +3,9 @@ package com.ynb.zarArrays;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * сделать циклическое тестирование программы
+ */
 public class uniTest {
     public static void main(String[] args) {
         for (int i = 0; i < 2; i++) {
