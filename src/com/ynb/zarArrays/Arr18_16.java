0b2d9f1a42a6a9e33ac16d0964c8fc32901d155e
ynb925 <ynb4mail@gmail.com>
2022-06-17 17:46:09 +0300

    wrk done \\  no QA pass !!!

--- src/com/ynb/zarArrays/Arr18_16.java
+++ src/com/ynb/zarArrays/Arr18_16.java
@@ -20,23 +20,24 @@ public class Arr18_16 {
         System.out.println(" wrk cycle count: ");
         int[] arrSize = new int[scanner.nextInt()];
 
-
+        System.out.println(" print all arr nums ");
         for (int i = 0; i < arrSize.length; i++) {
             arrSize[i] = random.nextInt(10, 100);
-            System.out.println("test" + i + "| " + arrSize[i]);
+            System.out.println("index: " + i + "| " + arrSize[i]);
         }
+        System.out.println();
         System.out.println(" print res: even nums ");
         for (int e = 0; e < arrSize.length; e++) {
             if (arrSize[e] % 2 == 0) {
-                System.out.println("even" + e + "| " + arrSize[e]);
+                System.out.println("even" + "index: " + e + "| " + arrSize[e]);
             }
         }
+        System.out.println();
         System.out.println(" print res: odd nums  ");
         for (int o = 0; o < arrSize.length; o++) {
             if (arrSize[o] % 2 != 0) {
-                System.out.println("odd" + o + "| " + arrSize[o]);
+                System.out.println("odd" + "index: " + o + "| " + arrSize[o]);
             }
-
         }
     }
 }
