948b07f130c00c10f6631e80a928e89a3870b595
ynb925 <ynb4mail@gmail.com>
2022-06-18 00:37:09 +0300

    wrk done \\  no QA pass !!!

--- src/com/ynb/zarArrays/Arr18_17.java
+++ src/com/ynb/zarArrays/Arr18_17.java
@@ -1,5 +1,8 @@
 package com.ynb.zarArrays;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Задание 18.17 Напишите класс, который заполняет случайными трехзначными числами массив,
  * размер которого при инициализации вводится с клавиатуры. После заполнения класс должен
@@ -9,4 +12,18 @@ package com.ynb.zarArrays;
  * Например, массив 345, 455, 677, 175, 985 выводится на экран следующим образом: 345 и 985 455и175 677
  */
 public class Arr18_17 {
+    public static void main(String[] args) {
+
+        Scanner arrSizeNum = new Scanner(System.in);
+        Random random = new Random();
+
+        int[] arr = new int[arrSizeNum.nextInt()];
+
+        System.out.println(" print all arr nums ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100, 1000);
+            System.out.println("index: " + i + "| " + arr[i]);
+        }
+
+    }
 }
