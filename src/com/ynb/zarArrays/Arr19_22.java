1b95efc13679165f35b88c641c69fa05512b78f2
ynb925 <ynb4mail@gmail.com>
2022-06-22 17:04:47 +0300

    prg wrk \\\ qa    pass

--- src/com/ynb/zarArrays/Arr19_22.java
+++ src/com/ynb/zarArrays/Arr19_22.java
@@ -1,11 +1,38 @@
 package com.ynb.zarArrays;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Задание 19.22 Напишите метод, который принимает два параметра: массив целых чисел М и еще одно целое число Х.
  * Метод возвращает количество ячеек массива М, содержащих значение Х (число появлений Х в М).
+ * input {1,2,3,4} , int x=23   ||   output prt : 4 cell and num =23
  */
 public class Arr19_22 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int[] arr = new int[scanner.nextInt()];
+        int Xnum = scanner.nextInt();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(10, 20);
+            System.out.println(arr[i] + " ");
+            System.out.println("--------------------------------");
+        }
+        System.out.println(arrNums(arr, Xnum));
+    }
+
+    public static int arrNums(int[] array, int value) {
+        int counter = 0;
+
+        for (int j : array) {
+            if (value == j) {
+                counter++;
+            }
+        }
+        return counter;
     }
 }
--- src/com/ynb/zarArrays/Arr19_23.java
+++ src/com/ynb/zarArrays/Arr19_23.java
@@ -1,5 +1,10 @@
 package com.ynb.zarArrays;
 
+import java.util.Random;
+import java.util.Scanner;
+
+import static com.ynb.zarArrays.Arr19_22.arrNums;
+
 /**
  * Задание 19.23 Напишите метод, который принимает два параметра:
  * массив целых чисел М и еще одно целое число Х.
@@ -8,5 +13,27 @@ package com.ynb.zarArrays;
 public class Arr19_23 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int[] arr = new int[scanner.nextInt()];
+        int Xnum = scanner.nextInt();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(10, 20);
+            System.out.println(arr[i] + " ");
+            System.out.println("--------------------------------");
+        }
+        System.out.println(arrNums(arr, Xnum));
+    }
+
+    public static boolean arrNums(int[] array, int value) {
+
+        for (int j : array) {
+            if (value == j) {
+                return true;
+            }
+        }
+        return false;
     }
 }
