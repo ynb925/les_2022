e7951e753cc5177cdc56e863cf2bd9cbb8315345
ynb925 <ynb4mail@gmail.com>
2022-06-22 17:49:20 +0300

    prg wrk \\\ qa    pass

--- src/com/ynb/zarArrays/Arr19_20.java
+++ src/com/ynb/zarArrays/Arr19_20.java
@@ -6,12 +6,17 @@ import static com.ynb.zarArrays.CreateArray.*;
 
 /**
  * Задание 19.20 Напишите метод, который получает в качестве параметра
- * массив и проверяет есть ли там повторяющиеся элементы.
+ * массив и проверяет есть ли там повторяющиеся элемент ВЩЩБЩЕ.
  * Если есть - возвращает TRUE
+ * <p>
+ * <p>
+ * 1 2 3 6 8 4 6 1 515 893
  */
 public class Arr19_20 {
     public static void main(String[] args) {
-        int[] arrSize = createArr(10, 30);
+        //  int[] arrSize = createArr(10, 30);
+
+        int[] arrSize = {1, 2, 3, 4, 5, 6, 2, 8, 9, 11};
 
         printArr(arrSize);
         System.out.println(equalNumArr(arrSize));
@@ -19,11 +24,24 @@ public class Arr19_20 {
     }
 
     public static boolean equalNumArr(int[] array) {
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] == array[i - 1]) {
-                return true;
+
+        for (int i = 0; i < array.length; i++) {    //  0
+            for (int j = array.length - 1; j > array.length; j--) {  // 1
+                if (array[i] == array[j]) {
+                    return true;
+                }
             }
         }
         return false;
     }
 }
+
+/*
+        for (int i = 0, j = i + 1; i < array.length - 1; i++, j++) {
+            if (array[j] == array[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}*/
