8b6c461a6ca3d28a05520a169f2a92e56759699c
ynb925 <ynb4mail@gmail.com>
2022-06-28 00:14:54 +0300

    prg wrk \\\ qa    pass

--- src/com/ynb/javaRushTask/javaRush_3.java
+++ src/com/ynb/javaRushTask/javaRush_3.java
@@ -1,11 +1,44 @@
 package com.ynb.javaRushTask;
 
 /**
- *
+ * Правильный порядок
+ * Напишем утилиту для работы с массивами. Основная часть функционала готова: метод printArray() выводит в консоли все элементы массива.
+ * Тебе осталась мелочь: реализовать метод reverseArray(). Он должен менять порядок элементов массива на обратный.
+ * Метод должен работать только с массивами целочисленных значений (int[]).
+ * <p>
+ * Пример:
+ * Если массив содержал элементы:
+ * 1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+ * то после вызова метода reverseArray() должен содержать:
+ * 0, 9, 8, 7, 6, 5, 4, 3, 2, 1
+ * <p>
+ * <p>
+ * Requirements:
+ * 1. Метод reverseArray() должен менять порядок элементов массива на обратный.
  */
 
 public class javaRush_3 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        int[] array = {11, 22, 33, 44, 55, 66, 77, 88, 99};
+        printArray(array);
+        reverseArray(array);
+        printArray(array);
+    }
+
+    public static void reverseArray(int[] array) {
+        //напишите тут ваш код
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
     }
 }
