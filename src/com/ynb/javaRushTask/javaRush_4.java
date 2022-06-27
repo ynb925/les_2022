d2d3fdb4575583be27ba44a5e7f2344fcbc01547
ynb925 <ynb4mail@gmail.com>
2022-06-28 00:20:43 +0300

    prg wrk \\\ qa    pass

---
+++ src/com/ynb/javaRushTask/javaRush_4.java
@@ -0,0 +1,60 @@
+package com.ynb.javaRushTask;
+
+/**
+ * Универсальный солдат
+ * Неплохо было бы иметь один метод для решения различных задач. У тебя есть возможность написать такой.
+ * Создай еще 9 методов universalMethod(). В сумме их должно получиться 10.
+ * Параметры, которые они должны принимать, придумай сам.
+ * <p>
+ * <p>
+ * Requirements:
+ * 1. Программа должна содержать 10 методов с именем universalMethod.
+ */
+
+public class javaRush_4 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+
+    //напишите тут ваш код
+
+    public static void universalMethod(int a){
+
+    }
+
+    public static void universalMethod(int a, int b){
+
+    }
+
+    public static void universalMethod(double a){
+
+    }
+
+    public static void universalMethod(double a, double b){
+
+    }
+
+    public static void universalMethod(char a, char b){
+
+    }
+
+    public static void universalMethod(String a, String b){
+
+    }
+
+    public static void universalMethod(boolean a){
+
+    }
+
+    public static void universalMethod(boolean a, boolean b){
+
+    }
+
+    public static void universalMethod(long a){
+
+    }
+}
