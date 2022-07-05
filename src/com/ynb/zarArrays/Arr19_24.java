a90761729cc11162cc6cf40ff6854f54e3a1041e
ynb925 <ynb4mail@gmail.com>
2022-07-05 13:07:23 +0300

    prg wrk \\\ qa    pass

---
+++ src/com/ynb/zarArrays/Arr19_24.java
@@ -0,0 +1,35 @@
+package com.ynb.zarArrays;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Задание 19.24 Напишите метод, который принимает в качестве параметра массив целых чисел и проверяет,
+ * являются ли все элементы массива положительными числами.
+ */
+public class Arr19_24 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.println(" wrt arr size:");
+        int[] arr = new int[scanner.nextInt()];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(-10, 10);
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println("--------------------------------");
+        System.out.println(arrСontain(arr));
+    }
+
+    public static boolean arrСontain(int[] array) {
+
+        for (int j : array) {
+            if (0 <= j) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
