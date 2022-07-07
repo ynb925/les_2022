c4d90d9d171bcbedce1e7f4604569336be910317
ynb925 <ynb4mail@gmail.com>
2022-07-07 14:37:29 +0300

    prg wrk \\\ qa    pass

--- src/com/ynb/zarArrays/Arr19_40.java
+++ src/com/ynb/zarArrays/Arr19_40.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 
 public class Arr19_40 {
     public static void main(String[] args) {
-        int[] arr = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr = new int[]{1, 3, 6, 2, 7};
         System.out.println(Arrays.toString(arrReWrite(arr)));
     }
 
