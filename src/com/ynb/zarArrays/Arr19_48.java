13d3aa19043b388b595f5f06bc975b2d68f9be21
ynb925 <ynb4mail@gmail.com>
2022-07-11 21:27:23 +0300

    prg wrk \\\ qa    pass

--- src/com/ynb/zarArrays/Arr19_48.java
+++ src/com/ynb/zarArrays/Arr19_48.java
@@ -3,28 +3,31 @@ package com.ynb.zarArrays;
 /**
  * Задание 19.48 Напишите метод, который принимает в качестве параметров два массива целых чисел и проверяет,
  * содержатся ли все значения первого массива во втором.
+ * <p>
+ * {1 4 3 2}     {5 8 1 6 4 2 9 3}       -> true
+ * <p>
+ * {1 2 3}        {5 1 8 3 99}              ->false
  */
 
 
 public class Arr19_48 {
     public static void main(String[] args) {
 
-        int[] arr1 = new int[]{1, 2, 3, 4, 5, 5};
-        int[] arr2 = new int[]{1, 2, 3, 4, 5, 5};
+        int[] arr1 = new int[]{1, 4, 3, 2};
+        int[] arr2 = new int[]{5, 1, 2, 6, 4, 2, 9, 3};
 
-        System.out.println(arrEqual(arr1, arr2));
+        System.out.println(arrEqualNums(arr1, arr2));
     }
 
-    private static boolean arrEqual(int[] array1, int[] array2) {
-
-        for (int i = 0; i < array1.length; i++) {
-            for (int m = i + 1, j = 0; m < array2.length; m++, j++) {
-
-                if (array1[j] == array2[m]) {
-                    return true;
-                }
+    private static boolean arrEqualNums(int[] array1, int[] array2) {
+        point:
+        for (int k : array1) {
+            for (int i : array2) {
+                if (k == i)
+                    continue point;
             }
+            return false;
         }
-        return false;
+        return true;
     }
 }
