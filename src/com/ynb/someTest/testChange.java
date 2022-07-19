dde1d2e108e1a96282d8d0b8674c7363d6d69fa4
ynb925 <ynb4mail@gmail.com>
2022-07-19 19:54:42 +0300

    make in main

---
+++ src/com/ynb/someTest/testChange.java
@@ -0,0 +1,11 @@
+package com.ynb.someTest;
+
+public class testChange {
+    public static void main(String[] args) {
+        int a = 8;
+        for (int i = 0; i < a; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
